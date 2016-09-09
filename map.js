d8f92e95bfa38bfc0d79cc7dfe1597a554851c9c
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-09-08 22:00:21 -0400

    mobile search test 3

--- map.js
+++ map.js
@@ -73,10 +73,27 @@ function trackTransforms(context){
     
 }
 
+function detectMobile() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
 $(window).resize(function() {
     // Reset variables
-
-    window.location.reload(false); 
+    if (!detectMobile()){
+        window.location.reload(false);
+    }
 });
 
 
