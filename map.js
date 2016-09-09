0442593b42852688cc22cc029731a1fc0cd06aac
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-09-08 20:06:32 -0400

    mobile search test

--- map.js
+++ map.js
@@ -73,6 +73,11 @@ function trackTransforms(context){
     
 }
 
+var $viewportMeta = $('meta[name="viewport"]');
+$('input, select, textarea').bind('focus blur', function(event) {
+$viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' +        (event.type == 'blur' ? 10 : 1));
+});
+
 $(window).resize(function() {
     // Reset variables
 
